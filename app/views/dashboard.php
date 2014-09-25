10c58e66aa70b7573a8383dc76234a613c1eaa01
srph <seraphipod@gmail.com>
2014-09-26 06:44:27 +0800

    (updated) index view. (added) dashboard view

---
+++ app/views/dashboard.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title ng-bind="'PLM Dashboard- ' + $state.current.rawTitle">PLM Dashboard</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <div ui-view></div>
+</body>
+</html>
---
+++ app/views/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title ng-bind="'Pamantasan ng Lungsod ng Maynila - ' + $state.current.rawTitle">Pamantasan ng Lungsod ng Maynila</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <div ui-view></div>
+</body>
+</html>
