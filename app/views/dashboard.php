2d2623d8773d0bc5a8e193735a981e2c61f196d0
srph <seraphipod@gmail.com>
2014-10-01 02:59:58 +0800

    (updated) views.

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -2,7 +2,10 @@
 <html>
 <head>
   <title ng-bind="'PLM Dashboard- ' + $state.current.rawTitle">PLM Dashboard</title>
+  <meta name="fragment" content="!">
   <meta charset="utf-8">
+
+  <base href="/">
 </head>
 <body>
   <div ui-view></div>
--- app/views/index.php
+++ app/views/index.php
@@ -2,7 +2,10 @@
 <html>
 <head>
   <title ng-bind="'Pamantasan ng Lungsod ng Maynila - ' + $state.current.rawTitle">Pamantasan ng Lungsod ng Maynila</title>
+  <meta name="fragment" content="!">
   <meta charset="utf-8">
+
+  <base href="/">
 </head>
 <body>
   <div ui-view></div>
